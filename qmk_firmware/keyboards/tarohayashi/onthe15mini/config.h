a3b37aecc35f97918fb0b52e8235418880dc1fcf
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2023-02-23 14:25:55 +0900

    code

---
+++ qmk_firmware/keyboards/tarohayashi/onthe15mini/config.h
@@ -0,0 +1,11 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//#include "config_common.h"
+
+/* RGB MATRIX */
+#define RGB_DI_PIN D2
+#define RGB_MATRIX_LED_COUNT 60
+#define RGB_MATRIX_KEYPRESSES
